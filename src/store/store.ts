e6fdc5d6f054f99c94c83022b6de702a8873abd5
bin <rnflrnfl41@naver.com>
2025-08-07 23:24:50 +0900

    feat: redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import messageSlice from "./messageSlice";
+import loadingSlice from './loadingSlice';
 
 const store = configureStore({
   reducer: {
     infoMessage: messageSlice,
+    loading: loadingSlice,
   },
 });
 
